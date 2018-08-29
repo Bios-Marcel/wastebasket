8b3f428fc39029a2b18117ad432d9e95016ac6f8
Bios-Marcel <marceloschr@googlemail.com>
2018-08-29 21:33:20 +0200

    Add intial windows version

--- wastebasket_windows.go
+++ wastebasket_windows.go
@@ -2,14 +2,30 @@
 
 package wastebasket
 
-import "errors"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
 
 //Trash moves a files or folder including its content into the systems trashbin.
 func Trash(path string) error {
-	return errors.New("Not supported yet")
+	file, fileError := os.Stat(path)
+	if fileError != nil {
+		return fileError
+	}
+
+	psCommand := ""
+	if file.IsDir() {
+		psCommand = fmt.Sprintf("\"Add-Type -AssemblyName Microsoft.VisualBasic;[Microsoft.VisualBasic.FileIO.FileSystem]::DeleteFolder(%s, 'OnlyErrorDialogs','SendToRecycleBin')\"", path)
+	} else {
+		psCommand = fmt.Sprintf("\"Add-Type -AssemblyName Microsoft.VisualBasic;[Microsoft.VisualBasic.FileIO.FileSystem]::DeleteFile(%s, 'OnlyErrorDialogs','SendToRecycleBin')\"", path)
+	}
+
+	return exec.Command("powershell", "-Command", psCommand).Run()
 }
 
 //Empty clears the platforms trashbin.
 func Empty() error {
-	return errors.New("Not supported yet")
+	exec.Command("powershell", "-Command", "\"Clear-RecycleBin\"")
 }
