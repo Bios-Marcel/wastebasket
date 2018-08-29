431151d1075dde0d71487211b0c3150de6bf6b74
Bios-Marcel <marceloschr@googlemail.com>
2018-08-29 21:58:25 +0200

    Improve unit tests

--- wastebasket_test.go
+++ wastebasket_test.go
@@ -7,15 +7,67 @@ import (
 )
 
 var testFilePath = "test.txt"
+var testDirPath = "test-delete-me"
 
 //TestTrash tests trashing a single file which is created beforehand
-func TestTrash(t *testing.T) {
+func TestTrashWithExistentFile(t *testing.T) {
+	writeTestFile(t)
 	error := Trash(testFilePath)
 	if error != nil {
 		t.Errorf("Error trashing file. (%s)", error)
 	}
 
 	_, error = os.Stat(testFilePath)
+	if os.IsExist(error) {
+		t.Errorf("File hasn't been deleted. (%s)", error)
+	}
+}
+
+//TestTrash tests trashing a single file which doesn't exist
+func TestTrashWithNonexistentFile(t *testing.T) {
+	_, error := os.Stat(testFilePath)
+	if os.IsExist(error) {
+		t.Errorf("File shouldn'T exist at start of this test. (%s)", error)
+	}
+
+	error = Trash(testFilePath)
+	if error != nil {
+		t.Errorf("Error trashing file. (%s)", error)
+	}
+
+	_, error = os.Stat(testFilePath)
+	if os.IsExist(error) {
+		t.Errorf("File hasn't been deleted. (%s)", error)
+	}
+}
+
+//TestTrash tests trashing a single file which is created beforehand
+func TestTrashWithExistentFolder(t *testing.T) {
+	writeTestDirectory(t)
+	error := Trash(testDirPath)
+	if error != nil {
+		t.Errorf("Error trashing file. (%s)", error)
+	}
+
+	_, error = os.Stat(testDirPath)
+	if os.IsExist(error) {
+		t.Errorf("File hasn't been deleted. (%s)", error)
+	}
+}
+
+//TestTrash tests trashing a single file which doesn't exist
+func TestTrashWithNonexistentFolder(t *testing.T) {
+	_, error := os.Stat(testDirPath)
+	if os.IsExist(error) {
+		t.Errorf("File shouldn'T exist at start of this test. (%s)", error)
+	}
+
+	error = Trash(testDirPath)
+	if error != nil {
+		t.Errorf("Error trashing file. (%s)", error)
+	}
+
+	_, error = os.Stat(testDirPath)
 	if os.IsExist(error) {
 		t.Errorf("File hasn't been deleted. (%s)", error)
 	}
@@ -29,6 +81,16 @@ func TestEmpty(t *testing.T) {
 	}
 }
 
-func writeTestFile() {
-	ioutil.WriteFile(testFilePath, []byte("Test"), os.ModePerm)
+func writeTestFile(t *testing.T) {
+	writeError := ioutil.WriteFile(testFilePath, []byte("Test"), os.ModePerm)
+	if writeError != nil {
+		t.Errorf("Error writing testfile. (%s)", writeError)
+	}
+}
+
+func writeTestDirectory(t *testing.T) {
+	mkdirError := os.Mkdir(testDirPath, os.ModePerm)
+	if mkdirError != nil {
+		t.Errorf("Error creating test directory. (%s)", mkdirError)
+	}
 }
