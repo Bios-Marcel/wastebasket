a1eeab9a361648cc0333508442acab806abadff0
Marcel Schramm <marceloschr@googlemail.com>
2023-01-01 19:25:32 +0100

    Implement file deletion on windows using WinAPI

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Bios-Marcel/wastebasket
 
 go 1.13
+
+require golang.org/x/sys v0.3.0
--- wastebasket_windows.go
+++ wastebasket_windows.go
@@ -4,31 +4,118 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
 )
 
-//Trash moves a file or folder including its content into the systems trashbin.
-func Trash(path string) error {
-	file, fileError := os.Stat(path)
+var (
+	shell32DLL         = windows.NewLazyDLL("shell32.dll")
+	shFileOperationW   = shell32DLL.NewProc("SHFileOperationW")
+	shEmptyRecycleBinW = shell32DLL.NewProc("SHEmptyRecycleBinW")
+)
 
-	if os.IsNotExist(fileError) {
-		return nil
-	}
+/*
+#define FO_DELETE         0x0003
 
-	if fileError != nil {
-		return fileError
-	}
+#define FOF_SILENT                 0x0004
+#define FOF_NOCONFIRMATION         0x0010
+#define FOF_ALLOWUNDO              0x0040
+#define FOF_NOCONFIRMMKDIR         0x0200
+#define FOF_NOERRORUI              0x0400
+#define FOF_NO_UI                  (FOF_SILENT | FOF_NOCONFIRMATION | FOF_NOERRORUI | FOF_NOCONFIRMMKDIR)
 
-	psCommand := ""
-	if file.IsDir() {
-		psCommand = fmt.Sprintf("Add-Type -AssemblyName Microsoft.VisualBasic;[Microsoft.VisualBasic.FileIO.FileSystem]::DeleteDirectory('%s', 'OnlyErrorDialogs','SendToRecycleBin')", path)
-	} else {
-		psCommand = fmt.Sprintf("Add-Type -AssemblyName Microsoft.VisualBasic;[Microsoft.VisualBasic.FileIO.FileSystem]::DeleteFile('%s', 'OnlyErrorDialogs','SendToRecycleBin')", path)
-	}
+typedef struct _SHFILEOPSTRUCTW {
+  HWND         hwnd;
+  UINT         wFunc;
+  PCZZWSTR     pFrom;
+  PCZZWSTR     pTo;
+  FILEOP_FLAGS fFlags;
+  BOOL         fAnyOperationsAborted;
+  LPVOID       hNameMappings;
+  PCWSTR       lpszProgressTitle;
+} SHFILEOPSTRUCTW, *LPSHFILEOPSTRUCTW;
+*/
 
-	return exec.Command("powershell", "-Command", psCommand).Run()
+const (
+	FO_DELETE = 0x0003
+
+	FOF_SILENT         = 0x0004
+	FOF_NOCONFIRMATION = 0x0010
+	FOF_ALLOWUNDO      = 0x0040
+	FOF_NOCONFIRMMKDIR = 0x0200
+	FOF_NOERRORUI      = 0x0400
+	FOF_NO_UI          = FOF_SILENT | FOF_NOCONFIRMATION | FOF_NOERRORUI | FOF_NOCONFIRMMKDIR
+)
+
+type SHFileOpStructW struct {
+	// Irrelevant, as its for UI related things
+	hwnd windows.HWND
+
+	wFunc uint32
+	pFrom *uint16
+	// This isn't necessary as it is only needed for copy / move actions.
+	pTo    *uint16
+	fFlags uint16
+
+	// FIXME Why isn't this relevant?
+	fAnyOperationsAborted int32
+	// Irrelevant, as its for move operations
+	hNameMappings uintptr
+	// Irrelevant, as its for UI related things
+	lpszProgressTitle *uint16
 }
 
-//Empty clears the platforms trashbin.
+// Trash moves a file or folder including its content into the systems trashbin.
+func Trash(path string) error {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return nil
+	} else if err != nil {
+		return err
+	}
+
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		return err
+	}
+
+	fileName, err := makeDoubleNullTerminatedLpstr(absPath)
+	if err != nil {
+		return fmt.Errorf("error creating utf16ptr for passed path: %w", err)
+	}
+
+	ret, _, err := shFileOperationW.Call(uintptr(unsafe.Pointer(&SHFileOpStructW{
+		hwnd:                  windows.HWND(0),
+		wFunc:                 FO_DELETE,
+		pFrom:                 fileName,
+		pTo:                   nil,
+		fFlags:                FOF_NO_UI | FOF_ALLOWUNDO,
+		fAnyOperationsAborted: 0,
+		hNameMappings:         0,
+		lpszProgressTitle:     nil,
+	})))
+
+	if ret != 0 {
+		return fmt.Errorf("windows error: %w", err)
+	}
+
+	return nil
+}
+
+func makeDoubleNullTerminatedLpstr(items ...string) (*uint16, error) {
+	chars := []uint16{}
+	for _, s := range items {
+		converted, err := windows.UTF16FromString(s)
+		if err != nil {
+			return nil, err
+		}
+		chars = append(chars, converted...)
+	}
+	chars = append(chars, 0)
+	return &chars[0], nil
+}
+
 func Empty() error {
 	return exec.Command("powershell", "-Command", "\"Clear-RecycleBin\"").Run()
 }
