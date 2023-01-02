b81824557ef14a2f73b7a4d5787b0e7da614f580
Marcel Schramm <marceloschr@googlemail.com>
2023-01-02 13:06:00 +0100

    Write tests for seperate tools used under linux

--- wastebasket_linux.go
+++ wastebasket_linux.go
@@ -6,19 +6,59 @@ import (
 	"os/exec"
 )
 
+var (
+	availabilityCache   = make(map[string]bool)
+	errToolNotAvailable = errors.New("tool not available")
+)
+
 func isCommandAvailable(name string) bool {
+	avail, ok := availabilityCache[name]
+	if avail && ok {
+		return true
+	}
 	_, fileError := exec.LookPath(name)
+	availabilityCache[name] = fileError == nil
 	return fileError == nil
 }
 
-//Trash moves a file or folder including its content into the systems trashbin.
+// Trash moves a file or folder including its content into the systems trashbin.
 func Trash(path string) error {
-	//gio us the tool that replaces gvfs, therefore it is the first choice.
+	//gio is the tool that replaces gvfs, therefore it is the first choice.
+	if err := gioTrash(path); err != nil && err != errToolNotAvailable {
+		return err
+	}
+
+	if err := gvfsTrash(path); err != nil && err != errToolNotAvailable {
+		return err
+	}
+
+	if err := trashCli(path); err != nil && err != errToolNotAvailable {
+		return err
+	}
+
+	return errors.New("None of the commands `gio`, `gvfs-trash` or `trash` are available")
+}
+
+func gioTrash(path string) error {
 	if isCommandAvailable("gio") {
+		// --force makes sure we don't get errors for non-existent files.
 		return exec.Command("gio", "trash", "--force", path).Run()
-	} else if isCommandAvailable("gvfs-trash") {
+	}
+
+	return errToolNotAvailable
+}
+
+func gvfsTrash(path string) error {
+	if isCommandAvailable("gvfs-trash") {
+		// --force makes sure we don't get errors for non-existent files.
 		return exec.Command("gvfs-trash", "--force", path).Run()
-	} else if isCommandAvailable("trash") {
+	}
+
+	return errToolNotAvailable
+}
+
+func trashCli(path string) error {
+	if isCommandAvailable("trash") {
 		//trash-cli throws 74 in case the file doesn't exist
 		_, fileError := os.Stat(path)
 
@@ -29,19 +69,47 @@ func Trash(path string) error {
 		return exec.Command("trash", "--", path).Run()
 	}
 
-	return errors.New("None of the commands `gio`, `gvfs-trash` or `trash` are available")
+	return errToolNotAvailable
 }
 
-//Empty clears the platforms trashbin.
+// Empty clears the platforms trashbin.
 func Empty() error {
-	//gio us the tool that replaces gvfs, therefore it is the first choice.
-	if isCommandAvailable("gio") {
-		return exec.Command("gio", "trash", "--empty").Run()
-	} else if isCommandAvailable("gvfs-trash") {
-		return exec.Command("gvfs-trash", "--empty").Run()
-	} else if isCommandAvailable("trash-empty") {
-		return exec.Command("trash-empty").Run()
+	//gio is the tool that replaces gvfs, therefore it is the first choice.
+	if err := gioEmpty(); err != nil && err != errToolNotAvailable {
+		return err
+	}
+
+	if err := gvfsEmpty(); err != nil && err != errToolNotAvailable {
+		return err
+	}
+
+	if err := trashCliEmpty(); err != nil && err != errToolNotAvailable {
+		return err
 	}
 
 	return errors.New("None of the commands `gio`, `gvfs-trash` or `trash-empty` are available")
+}
+
+func gioEmpty() error {
+	if isCommandAvailable("gio") {
+		return exec.Command("gio", "trash", "--empty").Run()
+	}
+
+	return errToolNotAvailable
+}
+
+func gvfsEmpty() error {
+	if isCommandAvailable("gvfs-trash") {
+		return exec.Command("gvfs-trash", "--empty").Run()
+	}
+
+	return errToolNotAvailable
+}
+
+func trashCliEmpty() error {
+	if isCommandAvailable("trash-empty") {
+		return exec.Command("trash-empty").Run()
+	}
+
+	return errToolNotAvailable
 }
---
+++ wastebasket_linux_test.go
@@ -0,0 +1,93 @@
+//go:build linux
+
+package wastebasket
+
+import (
+	"os"
+	"testing"
+)
+
+func Test_Empty_Gio(t *testing.T) {
+	err := gioEmpty()
+	if err == errToolNotAvailable {
+		t.SkipNow()
+	}
+
+	if err != nil {
+		t.Errorf("unexpected error clearing trash: %s", err)
+	}
+}
+
+func Test_Empty_Gvfs(t *testing.T) {
+	err := gvfsEmpty()
+	if err == errToolNotAvailable {
+		t.SkipNow()
+	}
+
+	if err != nil {
+		t.Errorf("unexpected error clearing trash: %s", err)
+	}
+}
+
+func Test_Empty_TrashCli(t *testing.T) {
+	err := trashCliEmpty()
+	if err == errToolNotAvailable {
+		t.SkipNow()
+	}
+
+	if err != nil {
+		t.Errorf("unexpected error clearing trash: %s", err)
+	}
+}
+
+func Test_Trash_DifferentTools(t *testing.T) {
+	type testCase struct {
+		name     string
+		path     string
+		fnDelete func(string) error
+		err      error
+	}
+
+	cases := []testCase{
+		{
+			name:     "basic gio test",
+			path:     testFilePath,
+			fnDelete: gioTrash,
+			err:      nil,
+		},
+		{
+			name:     "basic gvfs test",
+			path:     testFilePath,
+			fnDelete: gvfsTrash,
+			err:      nil,
+		},
+		{
+			name:     "basic trashCli test",
+			path:     testFilePath,
+			fnDelete: trashCli,
+			err:      nil,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			defer cleanup()
+			writeTestFile(c.path, t)
+
+			err := c.fnDelete(c.path)
+			if err == errToolNotAvailable {
+				t.SkipNow()
+			}
+
+			if err != c.err {
+				t.Errorf("unexpected error: %v != %v", err, c.err)
+			}
+
+			if _, err := os.Stat(c.path); os.IsNotExist(err) {
+				//Everything correct!
+			} else {
+				t.Errorf("File hasn't been deleted.")
+			}
+		})
+	}
+}
