a366c3b83b765c22b9a92f9e919c6ebc8dce20d7
Marcel Schramm <marceloschr@googlemail.com>
2023-02-04 19:24:53 +0100

    Make sure consecutive benchmark runs dont make the benchmark slower

--- wastebasket_nix_bench_test.go
+++ wastebasket_nix_bench_test.go
@@ -6,12 +6,16 @@ import (
 	"fmt"
 	"os"
 	"testing"
+	"time"
 )
 
 func create_files(count int) []string {
+	// Prevent slowdowns of consecutive runs, since we have to check for file
+	// existence more often if we create the file multiple times.
+	timeNow := time.Now().Format(time.RFC3339)
 	files := make([]string, 0, count)
 	for i := 1; i <= count; i++ {
-		path := fmt.Sprintf("./%d.txt", i)
+		path := fmt.Sprintf("./%s-%d.txt", timeNow, i)
 		files = append(files, path)
 		err := os.WriteFile(path, []byte("test"), os.ModePerm)
 		if err != nil {
