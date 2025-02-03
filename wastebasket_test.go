2f40f460c3419b4b68adc4b0c509c79c12f20b8a
Marcel Schramm <marceloschr@googlemail.com>
2025-02-03 16:38:23 +0100

    Fix tests

--- wastebasket_test.go
+++ wastebasket_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/Bios-Marcel/wastebasket/v2/wastebasket_nix"
 	"github.com/Bios-Marcel/wastebasket/v2/wastebasket_windows"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 var (
@@ -194,7 +195,7 @@ func Test_Query_Restore_Homedir(t *testing.T) {
 		path := filepath.Join(home, "path.txt")
 
 		for i := range 2 {
-			t.Cleanup(writeTestData(t, strconv.FormatInt(int64(i), 10), path))
+			t.Cleanup(writeTestDataWith(t, strconv.FormatInt(int64(i), 10), path))
 			assertExists(t, path)
 			if err := wastebasket.Trash(path); err != nil {
 				t.Errorf("Error trashing file. (%s)", err.Error())
@@ -203,32 +204,41 @@ func Test_Query_Restore_Homedir(t *testing.T) {
 		}
 
 		result, err := wastebasket.Query(wastebasket.QueryOptions{Search: []string{path}})
-		if assert.NoError(t, err) {
-			if assert.Len(t, result.Matches[path], 2) {
-				matchOne := result.Matches[path][0]
-				if assert.NoError(t, result.Matches[path][0].Restore(false)) {
-					f, err := os.ReadFile(matchOne.OriginalPath())
-					if assert.NoError(t, err) {
-						assert.Equal(t, "0", string(f))
-					}
-					assertExists(t, path)
-				}
-				if assert.Error(t, result.Matches[path][1].Restore(false), wastebasket.ErrAlreadyExists) {
-					f, err := os.ReadFile(matchOne.OriginalPath())
-					if assert.NoError(t, err) {
-						assert.Equal(t, "0", string(f))
-					}
-					assertExists(t, path)
-				}
-				if assert.NoError(t, result.Matches[path][1].Restore(true)) {
-					f, err := os.ReadFile(matchOne.OriginalPath())
-					if assert.NoError(t, err) {
-						assert.Equal(t, "1", string(f))
-					}
-					assertExists(t, path)
-				}
-			}
+		require.NoError(t, err)
+		require.Len(t, result.Matches[path], 2)
+		matchOne := result.Matches[path][0]
+		require.NoError(t, matchOne.Restore(false))
+		f, err := os.ReadFile(matchOne.OriginalPath())
+		require.NoError(t, err)
+		var wasFirst bool
+		if string(f) != "0" && string(f) != "1" {
+			t.Fatalf("File content unexpected: %s", string(f))
 		}
+		if string(f) == "0" {
+			wasFirst = true
+		}
+
+		assertExists(t, path)
+		matchTwo := result.Matches[path][1]
+		require.Error(t, matchTwo.Restore(false), wastebasket.ErrAlreadyExists)
+		f, err = os.ReadFile(matchTwo.OriginalPath())
+		require.NoError(t, err)
+		if wasFirst {
+			require.Equal(t, "0", string(f))
+		} else {
+			require.Equal(t, "1", string(f))
+		}
+
+		assertExists(t, path)
+		require.NoError(t, matchTwo.Restore(true))
+		f, err = os.ReadFile(matchTwo.OriginalPath())
+		require.NoError(t, err)
+		if wasFirst {
+			require.Equal(t, "1", string(f))
+		} else {
+			require.Equal(t, "0", string(f))
+		}
+		assertExists(t, path)
 	})
 }
 
